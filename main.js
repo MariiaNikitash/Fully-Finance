238dbfaa9c8d143dd25d0c1149af6b005337665c
owen-rote <rotenbergowen@csu.fullerton.edu>
2024-04-04 23:08:28 -0700

    fix: Prevent logic error in large remainder calculations

--- main.js
+++ main.js
@@ -1,56 +1,86 @@
+// Event: Submit
 $(document).ready(function () {
   $("#myButton").on("click", function (event) {
-    event.preventDefault(); //prevents reload
+    event.preventDefault(); //Prevents reload
 
-    calcRemaining();
+    // Calculate amount remaining
+    var remaining = calcRemaining();
 
-    calcFV();
-  }); 
+    // Grab year on slider
+    var year_on_slider = parseInt($("#YearCount").html());
 
-  $(".YearSlider").on("input", function (event) {
-    event.preventDefault();
-
-    var yrs = $(this).val();
-    //console.log(yrs);
-
-    $("#YearCount").html(yrs);
-    //console.log(yrs);
-    //console.log(parseInt($("#YearCount").html()));
-    calcFV();
-    var new_value = parseInt($("#future_val").html());
+    // Calculate future value
+    calcFV(remaining, year_on_slider);
   });
-
-  function calcRemaining() {
-    var income = parseInt($("#income").val());
-    var rent = parseInt($("#rent").val());
-    var transportation = parseInt($("#transportation").val());
-    var utilities = parseInt($("#utilities").val());
-    var food = parseInt($("#food").val());
-    var healthcare = parseInt($("#healthcare").val());
-    var debt = parseInt($("#debt").val());
-    var personal = parseInt($("#personal").val());
-    var remaining =
-      income - (rent + transportation + utilities + food + healthcare + debt + personal);
-    remaining = numberWithCommas(remaining)
-    $("#remaining").html(remaining);
-  }
-
-  function calcFV() {
-    t = parseInt($("#YearCount").html());
-
-    //console.log(t);
-
-    var remaining = parseInt($("#remaining").html());
-    var c = remaining * 12;
-    var fv = (c * (Math.pow(1.1, t) - 1)) / 0.1;
-
-    fv = fv.toFixed(0);
-    fv = numberWithCommas(fv);
-    console.log(fv)
-    $("#future_val").html(fv);
-  }
-
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
 });
+
+// Event: Slider move
+$(".YearSlider").on("input", function (event) {
+  event.preventDefault(); //Prevents reload
+
+  // Grab year on slider
+  var year_on_slider = $(this).val();
+  // Set html attribute so site can display current year_on_slider
+  $("#YearCount").html(year_on_slider);
+
+  // Get remaining amount (str)(with commas) from html attribute
+  var remaining = $("#remaining").html();
+
+  // Remove commas and make int
+  remaining = parseInt(remaining.replace(",", ""));
+
+  // Calculate future value
+  calcFV(remaining, year_on_slider);
+});
+
+function calcRemaining() {
+  // Returns: (int) remaining
+
+  // Grab values from input boxes
+  var income = parseInt($("#income").val());
+  var rent = parseInt($("#rent").val());
+  var transportation = parseInt($("#transportation").val());
+  var utilities = parseInt($("#utilities").val());
+  var food = parseInt($("#food").val());
+  var healthcare = parseInt($("#healthcare").val());
+  var debt = parseInt($("#debt").val());
+  var personal = parseInt($("#personal").val());
+  var remaining =
+    income -
+    (rent + transportation + utilities + food + healthcare + debt + personal);
+
+  // Save the remaining amount as int (before comma separators are added)
+  var remaining_without_commas = remaining;
+
+  // Insert commas separators
+  remaining = numberWithCommas(remaining);
+
+  // Set html attribute to display remaining (format: xxx,xxx,xxx)
+  $("#remaining").html(remaining);
+
+  // Return remaining balance as int
+  return remaining_without_commas;
+}
+
+function calcFV(remaining, t) {
+  // Returns: None
+  // t        : Year on slider
+  // remaining: Remaining money
+
+  var contribution = remaining * 12;
+  var fv = (contribution * (Math.pow(1.1, t) - 1)) / 0.1;
+
+  // Format fv: Add commas and specify precision
+  fv = fv.toFixed(0);
+  fv = numberWithCommas(fv);
+
+  console.log("Calculated future value: ", fv);
+
+  // Set html attribute for display
+  $("#future_val").html(fv);
+}
+
+function numberWithCommas(x) {
+  // Takes int and inserts comma separators
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
